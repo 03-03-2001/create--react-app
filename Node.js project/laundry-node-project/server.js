06d34ebdd00e168fb3688edbe2014c9424e00893
03-03-2001 <rohitvkamble0301@gmail.com>
2026-05-23 02:03:14 +0530

    Fixed handleRoute export issue

--- Node.js project/laundry-node-project/server.js
+++ Node.js project/laundry-node-project/server.js
@@ -1,13 +1,15 @@
 const http = require("http");
 const handleRoute = require("./routes");
 
-const Port = 3000;
+const PORT = process.env.PORT || 3000;
+
+
 
 const server = http.createServer((req,res)=>{
    handleRoute(req,res);
 })
  
 
-server.listen(3000,(req,res)=>{
-    console.log("Server is Running at http://localhost:3000");
+server.listen(PORT, (req, res) => {
+    console.log(`Server is Running at http://localhost:${PORT}`);
 });
